435f2a3ec90f9f1b21e399aa02e0cf487473bb75
Mehmet Veysel Dilim <veyseldilim@gmail.com>
2024-03-02 02:34:44 +0300

    Valid Anagram

---
+++ array_hashing/Valid Anagram/Program.cs
@@ -0,0 +1,27 @@
+﻿bool IsAnagram(string s, string t)
+{
+    if (s.Length != t.Length)
+        return false;
+
+    int[] arr = new int[26];
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        int index = s[i] - 'a';
+        arr[index] += 1;
+    }
+
+    for (int j = 0; j < t.Length; j++)
+    {
+        int index = t[j] - 'a';
+        arr[index] -= 1;
+    }
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != 0)
+            return false;
+    }
+
+    return true;
+}
