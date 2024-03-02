af523bdb2130077b031e6381cdd1cf0fc240cba9
Mehmet Veysel Dilim <veyseldilim@gmail.com>
2024-03-02 03:03:07 +0300

    Valid Anagram Another Solution

--- array_hashing/Valid Anagram/Program.cs
+++ array_hashing/Valid Anagram/Program.cs
@@ -1,27 +1,58 @@
-﻿bool IsAnagram(string s, string t)
+﻿// bool IsAnagram(string s, string t)
+// {
+//     if (s.Length != t.Length)
+//         return false;
+
+//      if(s == t) return true;
+
+
+//     int[] arr = new int[26];
+
+//     for (int i = 0; i < s.Length; i++)
+//     {
+//         int index = s[i] - 'a';
+//         arr[index] += 1;
+//     }
+
+//     for (int j = 0; j < t.Length; j++)
+//     {
+//         int index = t[j] - 'a';
+//         arr[index] -= 1;
+//     }
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] != 0)
+//             return false;
+//     }
+
+//     return true;
+// }
+
+bool IsAnagram(string s, string t)
 {
     if (s.Length != t.Length)
         return false;
 
-    int[] arr = new int[26];
+    if (s == t)
+        return true;
+    Dictionary<char, int> charCounter = new();
 
     for (int i = 0; i < s.Length; i++)
     {
-        int index = s[i] - 'a';
-        arr[index] += 1;
+        if (!charCounter.ContainsKey(s[i]))
+            charCounter[s[i]] = 0;
+        if (!charCounter.ContainsKey(t[i]))
+            charCounter[t[i]] = 0;
+
+        charCounter[s[i]] += 1;
+        charCounter[t[i]] -= 1;
     }
 
-    for (int j = 0; j < t.Length; j++)
+    foreach (var (key, value) in charCounter)
     {
-        int index = t[j] - 'a';
-        arr[index] -= 1;
-    }
-
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] != 0)
+        if (value != 0)
             return false;
     }
-
     return true;
 }
