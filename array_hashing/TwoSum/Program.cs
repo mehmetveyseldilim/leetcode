72168748b5396fec0c131b7aa516e8c80cd2f9a3
Mehmet Veysel Dilim <veyseldilim@gmail.com>
2024-03-02 23:30:08 +0300

    TwoSum

---
+++ array_hashing/TwoSum/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+    int[] index = new int[2];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int requiredNumber = target - nums[i];
+
+        if (keyValuePairs.ContainsKey(nums[i]))
+        {
+            return new int[2] { keyValuePairs[nums[i]], i };
+        }
+
+        if (!keyValuePairs.ContainsKey(requiredNumber))
+        {
+            keyValuePairs.Add(requiredNumber, i);
+        }
+    }
+
+    return index;
+}
+Console.WriteLine("Hello, World!");
