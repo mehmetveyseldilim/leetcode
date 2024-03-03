38db1fb670490d0d15c138b5549c6a61c77b3dc5
Mehmet Veysel Dilim <veyseldilim@gmail.com>
2024-03-03 23:06:53 +0300

    IsPalindrome

--- create_project.sh
+++ create_project.sh
@@ -4,19 +4,27 @@ green="\033[1;32m"
 reset="\033[m"
 
 # Assign the directory name to a variable
-directory_name="array_hashing"
+directory_name="two_pointers"
 
-cd "$directory_name"
+# Check if the directory exists
+if [ ! -d "$directory_name" ]; then
+    echo -e "${green}Creating directory: $directory_name${reset}"
+    mkdir "$directory_name"
+else
+    echo -e "${green}Directory already exists: $directory_name${reset}"
+fi
+
+cd "$directory_name" || exit
 
 # Prompt the user for input
 read -p "Enter project name: " project_name
 
-echo -e "${green}Creating console projects${reset}"
+echo -e "${green}Creating console project: $project_name${reset}"
 dotnet new console -n "$project_name"
 
 cd ..
 
-echo -e "${green}Adding projects to the solution${reset}"
+echo -e "${green}Adding project to the solution${reset}"
 dotnet sln add "$directory_name/$project_name/$project_name.csproj"
 
 echo -e "${green}Executing dotnet restore${reset}"
---
+++ two_pointers/ValidPalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool IsPalindrome(string s)
+{
+    int leftIndex = 0;
+    int rightIndex = s.Length - 1;
+
+    while (leftIndex < rightIndex)
+    {
+        if (!char.IsLetterOrDigit(s[leftIndex]))
+        {
+            leftIndex++;
+        }
+        else if (!char.IsLetterOrDigit(s[rightIndex]))
+        {
+            rightIndex--;
+        }
+        else if (char.ToLower(s[leftIndex]) == char.ToLower(s[rightIndex]))
+        {
+            leftIndex++;
+            rightIndex--;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+Console.WriteLine("Hello, World!");
