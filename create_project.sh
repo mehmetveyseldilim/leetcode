1737e9483a30f9f143e7f0fc3a698fe28fe7595b
Mehmet Veysel Dilim <veyseldilim@gmail.com>
2024-03-17 17:14:38 +0300

    BestTimeToBuyOrSellStock

--- create_project.sh
+++ create_project.sh
@@ -4,7 +4,7 @@ green="\033[1;32m"
 reset="\033[m"
 
 # Assign the directory name to a variable
-directory_name="two_pointers"
+directory_name="sliding_window"
 
 # Check if the directory exists
 if [ ! -d "$directory_name" ]; then
---
+++ sliding_window/BestTimeToBuyOrSellStock/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Optimum O(N) Solution
+
+int MaxProfit(int[] prices)
+{
+    int maxProfit = 0;
+    int buy = prices[0];
+
+    for (int i = 1; i < prices.Length; i++)
+    {
+        int profit = prices[i] - buy;
+
+        if (profit > maxProfit)
+            maxProfit = profit;
+
+        if (buy > prices[i])
+        {
+            buy = prices[i];
+        }
+    }
+
+    return maxProfit;
+}
+
+/*
+
+O(N^2) Solution
+int MaxProfit(int[] prices)
+{
+    int maxProfit = 0;
+
+    for (int i = 0; i < prices.Length; i++)
+    {
+        for (int j = i + 1; j < prices.Length; j++)
+        {
+            int profit = prices[j] - prices[i];
+
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+}
+*/
+
+
+int[] ints = { 7, 6, 4, 3, 1 };
+
+Console.WriteLine(MaxProfit(ints));
